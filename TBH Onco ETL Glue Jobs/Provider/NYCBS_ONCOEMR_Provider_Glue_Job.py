66147160ec3559dac2d1f1c6aaf6e4744e93d02d
samson-m-bmc <samson.musumuri@bmc.org>
2024-07-29 11:56:46 -0400

    Updated sscripts for glue jobs

--- TBH Onco ETL Glue Jobs/Provider/NYCBS_ONCOEMR_Provider_Glue_Job.py
+++ TBH Onco ETL Glue Jobs/Provider/NYCBS_ONCOEMR_Provider_Glue_Job.py
@@ -1,3 +1,15 @@
+# ****************************************************************************
+# Copyright © 2024 LeapLogic.
+#
+# This is a copyrighted work.
+# This program and the information contained in it is confidential and proprietary 
+# to Clearway Health.
+# You are prohibited from making a copy or modification of, or from redistributing,
+# reproducing, rebroadcasting, or re-encoding of this content without the prior 
+# written consent of Clearway Health. 
+# ****************************************************************************
+
+
 import boto3
 import pandas as pd
 from io import StringIO
